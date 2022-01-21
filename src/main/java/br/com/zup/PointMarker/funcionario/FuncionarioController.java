e9833e0ad0dcf22ba73736afe456946d57ecd076
evellyinhelenoir <evellyin.nunes@zup.com.br>
2022-01-21 15:13:21 -0300

    Criar método de atualizar salário do funcionário por ID na Controller

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
@@ -2,6 +2,8 @@ package br.com.zup.PointMarker.funcionario;
 
 import br.com.zup.PointMarker.dtos.AtualizarSalarioEntradaDTO;
 import br.com.zup.PointMarker.dtos.AtualizarSalarioSaidaDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,13 +12,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/funcionario")
 public class FuncionarioController {
+
+    private FuncionarioService funcionarioService;
+    private ModelMapper modelMapper;
+
+    @Autowired
+    public FuncionarioController(FuncionarioService funcionarioService, ModelMapper modelMapper) {
+        this.funcionarioService = funcionarioService;
+        this.modelMapper = modelMapper;
+    }
 
     @PutMapping("/cargo/{id}")
     @ResponseStatus(HttpStatus.OK)
     public AtualizarSalarioSaidaDTO atualizarSalarioDTO (@PathVariable int id, @RequestBody AtualizarSalarioEntradaDTO atualizarSalarioEntradaDTO){
 
+        Funcionario funcionario = funcionarioService.buscarFuncionario(id);
+         if (funcionario != null){
+            funcionario.setSalario(atualizarSalarioEntradaDTO.getSalario());
+         }
+         return modelMapper.map(funcionario, AtualizarSalarioSaidaDTO.class);
     }
 }
