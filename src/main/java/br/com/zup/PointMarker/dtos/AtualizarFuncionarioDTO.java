f03a5416a46d95df84beb3964615ba0aa5d23860
evellyinhelenoir <evellyin.nunes@zup.com.br>
2022-01-21 14:33:30 -0300

    Criar DTO de Atualizar Funcionário.

---
+++ src/main/java/br/com/zup/PointMarker/dtos/AtualizarFuncionarioDTO.java
@@ -0,0 +1,16 @@
+package br.com.zup.PointMarker.dtos;
+
+import br.com.zup.PointMarker.cargo.Cargo;
+import br.com.zup.PointMarker.enums.Status;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class AtualizarFuncionarioDTO {
+
+    private Cargo cargo;
+    private Status status;
+}
