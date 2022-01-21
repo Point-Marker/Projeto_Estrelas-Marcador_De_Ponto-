b7a05d7063fc0959246e4a173e7c18063ce4cb89
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-21 11:25:58 -0300

    Primeiro Commit.

---
+++ src/main/java/br/com/zup/PointMarker/PointMarkerApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.PointMarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointMarkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointMarkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/PointMarker/PointMarkerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.PointMarker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointMarkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
