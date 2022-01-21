5207bd09545c9ca217c5ff7c4f14923c0a569cde
evellyinhelenoir <evellyin.nunes@zup.com.br>
2022-01-21 15:30:58 -0300

    Criar método de teste para buscador de funcionario

--- src/test/java/br/com/zup/PointMarker/funcionario/FuncionarioServiceTeste.java
+++ src/test/java/br/com/zup/PointMarker/funcionario/FuncionarioServiceTeste.java
@@ -2,13 +2,17 @@ package br.com.zup.PointMarker.funcionario;
 
 import br.com.zup.PointMarker.cargo.Cargo;
 import br.com.zup.PointMarker.enums.Status;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Optional;
 
 @SpringBootTest
 public class FuncionarioServiceTeste {
@@ -34,11 +38,19 @@ public class FuncionarioServiceTeste {
         cargo.setId(1);
 
         funcionario = new Funcionario();
+        funcionario.setId(1);
         funcionario.setNome("Afonso");
         funcionario.setCpf("159.307.330-58");
         funcionario.setDataDeNascimento(LocalDate.of(1999, Month.JULY, 12));
         funcionario.setSalario(cargo.getSalario());
         funcionario.setCargo(cargo);
         funcionario.setStatus(Status.ATIVO);
+    }
+
+    @Test
+    public void buscarFuncionarioPorIdTeste (){
+        Mockito.when(funcionarioRepository.findById(Mockito.anyInt())).thenReturn(Optional.ofNullable(funcionario));
+        Funcionario funcionarioEncontrado = funcionarioService.buscarFuncionario(1);
+        Assertions.assertNotEquals(funcionarioEncontrado, null);
     }
 }
