babc10874979f3872927eb8abee39c62d31e9524
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-21 12:33:24 -0300

    Criar Controller advisor.

---
+++ src/main/java/br/com/zup/PointMarker/config/configmensagem/ControllerAdvisor.java
@@ -0,0 +1,7 @@
+package br.com.zup.PointMarker.config.configmensagem;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvisor {
+}
