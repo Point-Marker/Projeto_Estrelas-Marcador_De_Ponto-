4098fb24ffb4189c7eecdfabe98108962496bd66
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-22 11:32:59 -0300

    Criar método de cadastro de funcionario.

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
@@ -1,9 +1,25 @@
 package br.com.zup.PointMarker.funcionario;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.PointMarker.funcionario.dtos.EntradaFuncionarioDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/funcionario")
 public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarFuncionario(@RequestBody EntradaFuncionarioDTO entradaFuncionarioDTO) {
+        Funcionario funcionario = modelMapper.map(entradaFuncionarioDTO, Funcionario.class);
+        funcionarioService.salvarFuncionario(funcionario);
+    }
 }
