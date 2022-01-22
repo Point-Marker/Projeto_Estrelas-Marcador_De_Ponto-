5273fb712b679c4e417c21d3796aa665dfa0a0d8
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-22 11:32:29 -0300

    Criar metodo para cadastrar funcionario.

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioService.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioService.java
@@ -1,7 +1,17 @@
 package br.com.zup.PointMarker.funcionario;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+    public Funcionario salvarFuncionario(Funcionario funcionario) {
+        return funcionarioRepository.save(funcionario);
+    }
+
+
 }
