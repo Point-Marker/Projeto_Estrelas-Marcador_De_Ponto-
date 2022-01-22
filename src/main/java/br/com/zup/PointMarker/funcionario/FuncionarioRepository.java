d8c4c4dff276b716dc1ad1004a92196c66673d84
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-22 11:03:40 -0300

    Criar interface para funcionario.

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioRepository.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.zup.PointMarker.funcionario;
 
 
-public class FuncionarioRepository {
+import org.springframework.data.repository.CrudRepository;
+
+public interface FuncionarioRepository extends CrudRepository<Funcionario, String> {
 }
