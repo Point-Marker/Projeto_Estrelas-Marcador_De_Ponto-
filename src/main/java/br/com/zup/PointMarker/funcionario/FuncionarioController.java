3d3a7d18324971d64c5c30e12354c4e8d376b96e
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2022-01-24 14:53:00 -0300

    AutoWired da FuncionarioService na FuncionarioController

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
@@ -1,9 +1,15 @@
 package br.com.zup.PointMarker.funcionario;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/funcionario")
 public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
 }
