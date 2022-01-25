5850707a9db7eca07c6c82d4532808a77f9a54f1
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-25 11:36:34 -0300

    Adicionar validacao na entrada de dados do funcionario para cadastro.

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
@@ -1,10 +1,13 @@
 package br.com.zup.PointMarker.funcionario;
 
 import br.com.zup.PointMarker.funcionario.dtos.EntradaFuncionarioDTO;
+import br.com.zup.PointMarker.funcionario.dtos.FuncionarioSaidaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/funcionario")
@@ -18,8 +21,9 @@ public class FuncionarioController {
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public void cadastrarFuncionario(@RequestBody EntradaFuncionarioDTO entradaFuncionarioDTO) {
+    public FuncionarioSaidaDTO cadastrarFuncionario(@RequestBody @Valid EntradaFuncionarioDTO entradaFuncionarioDTO) {
         Funcionario funcionario = modelMapper.map(entradaFuncionarioDTO, Funcionario.class);
         funcionarioService.salvarFuncionario(funcionario);
+        return modelMapper.map(funcionario, FuncionarioSaidaDTO.class);
     }
 }
