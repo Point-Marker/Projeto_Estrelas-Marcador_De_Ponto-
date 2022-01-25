2d65192327a2c3ffad7dcb89a78da13e7eba2389
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-25 11:37:45 -0300

    Testar cadastro de funcionario quando o cargo nao estiver cadastrado no sistema.

--- src/test/java/br/com/zup/PointMarker/funcionario/FuncionarioServiceTest.java
+++ src/test/java/br/com/zup/PointMarker/funcionario/FuncionarioServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.zup.PointMarker.funcionario;
 
 import br.com.zup.PointMarker.cargo.Cargo;
+import br.com.zup.PointMarker.cargo.CargoRepository;
 import br.com.zup.PointMarker.enums.Status;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,14 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @SpringBootTest
 public class FuncionarioServiceTest {
 
     @MockBean
     private FuncionarioRepository funcionarioRepository;
+
+    @MockBean
+    private CargoRepository cargoRepository;
 
     @Autowired
     private FuncionarioService funcionarioService;
@@ -38,7 +42,7 @@ public class FuncionarioServiceTest {
         funcionario = new Funcionario();
         funcionario.setNome("Afonso");
         funcionario.setCpf("159.307.330-58");
-        funcionario.setDataDeNascimento(LocalDate.of(1999, Month.JULY, 12));
+//        funcionario.setDataDeNascimento(LocalDate.of(1999, Month.JULY, 12));
         funcionario.setSalario(cargo.getSalario());
         funcionario.setCargo(cargo);
         funcionario.setStatus(Status.ATIVO);
@@ -46,14 +50,30 @@ public class FuncionarioServiceTest {
 
     @Test
     public void testarCadastrarFuncionarioCaminhoBom() {
+        Mockito.when(cargoRepository.findById(1)).thenReturn(Optional.of(cargo));
         Mockito.when(funcionarioRepository.save(Mockito.any(Funcionario.class))).thenReturn(funcionario);
 
         Funcionario funcionarioCadastrado = funcionarioService.salvarFuncionario(funcionario);
 
         Mockito.verify(funcionarioRepository, Mockito.times(1)).save(funcionario);
         Assertions.assertEquals(funcionarioCadastrado, funcionario);
-        System.out.println(funcionarioCadastrado);
-        System.out.println(funcionario);
+    }
+
+    @Test
+    public void testarCadastroDeFuncionarioCaminhoRuim_CargoNaoEncontrado() {
+        var cargoNaoCadastrado = new Cargo();
+        cargoNaoCadastrado.setId(2);
+
+        funcionario.setCargo(cargoNaoCadastrado);
+
+        Mockito.when(cargoRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
+
+        NoSuchElementException exception = Assertions.assertThrows(NoSuchElementException.class, () -> {
+            funcionarioService.salvarFuncionario(funcionario);
+        });
+
+        Mockito.verify(funcionarioRepository, Mockito.times(0)).save(funcionario);
+
     }
 
 }
