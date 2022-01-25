fff31aa0d2a1e4fb82553a65472637a073272585
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-25 11:48:37 -0300

    Customizar as mensagens de erro que aparecem para o usuario.

---
+++ src/main/java/br/com/zup/PointMarker/config/configmensagem/MensagemDeErro.java
@@ -0,0 +1,12 @@
+package br.com.zup.PointMarker.config.configmensagem;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class MensagemDeErro {
+    private String mensagemDeErro;
+}
