a5bef4e5a2d9d64e9dd56734729d92a8e97486db
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-25 15:50:53 -0300

    Incluir validacao do tamanho do nome minimo e maximo.

--- src/main/java/br/com/zup/PointMarker/funcionario/dtos/EntradaFuncionarioDTO.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/dtos/EntradaFuncionarioDTO.java
@@ -7,16 +7,20 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.br.CPF;
 
+import javax.persistence.Column;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class EntradaFuncionarioDTO {
+
     @NotBlank(message = "{nome.null}")
+    @Size(min = 20, max = 30, message = "{nome.size}")
     private String nome;
     @CPF
     private String cpf;
