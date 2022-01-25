b592fe1356ac67de1478eb80ca83863b96396f54
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-25 11:32:57 -0300

    Ajustar a forma de inserir dados para cadastro de funcionario.

--- src/main/java/br/com/zup/PointMarker/funcionario/dtos/EntradaFuncionarioDTO.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/dtos/EntradaFuncionarioDTO.java
@@ -1,21 +1,26 @@
 package br.com.zup.PointMarker.funcionario.dtos;
 
 import br.com.zup.PointMarker.cargo.Cargo;
-import br.com.zup.PointMarker.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class EntradaFuncionarioDTO {
+    @NotBlank(message = "{nome.null}")
     private String nome;
+    @CPF
     private String cpf;
-    private double salario;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataDeNascimento;
     private Cargo cargo;
 }
