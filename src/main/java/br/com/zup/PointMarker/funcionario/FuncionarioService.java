daf7aeb58a0efc4b26dea835af13dba9407e1f2b
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-25 11:35:18 -0300

    Verificar se cargo existe para sempre deixar o salario de acordo com o cargo.

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioService.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioService.java
@@ -1,17 +1,29 @@
 package br.com.zup.PointMarker.funcionario;
 
+import br.com.zup.PointMarker.cargo.Cargo;
+import br.com.zup.PointMarker.cargo.CargoRepository;
+import br.com.zup.PointMarker.enums.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class FuncionarioService {
 
     @Autowired
     private FuncionarioRepository funcionarioRepository;
+    @Autowired
+    private CargoRepository cargoRepository;
 
-    public Funcionario salvarFuncionario(Funcionario funcionario) {
-        return funcionarioRepository.save(funcionario);
+    public Funcionario salvarFuncionario(Funcionario entradafuncionario) {
+        Optional<Cargo> cargoOptional = cargoRepository.findById(entradafuncionario.getCargo().getId());
+        entradafuncionario.setCargo(cargoOptional.get());
+        cargoOptional.orElseThrow(NoSuchElementException::new);
+
+        entradafuncionario.setSalario(entradafuncionario.getCargo().getSalario());
+        entradafuncionario.setStatus(Status.ATIVO);
+        return funcionarioRepository.save(entradafuncionario);
     }
-
-
 }
