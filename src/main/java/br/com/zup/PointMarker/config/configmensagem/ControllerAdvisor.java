6042fbaad7cd2af8da19d5430b2297a20203c76a
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-25 16:38:37 -0300

    Modelar mensagens de erros vinda do json e do banco de dados.

--- src/main/java/br/com/zup/PointMarker/config/configmensagem/ControllerAdvisor.java
+++ src/main/java/br/com/zup/PointMarker/config/configmensagem/ControllerAdvisor.java
@@ -1,6 +1,8 @@
 package br.com.zup.PointMarker.config.configmensagem;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -34,4 +36,20 @@ public class ControllerAdvisor {
 
         return new MensagemDeErro("Este Cargo Não Está Cadastrado.");
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MensagemDeErro manipularMensagegParaDataDeNascimentoForaDoPadraoBrasileiro() {
+
+        return new MensagemDeErro("Digite a sua data de nascimento, " +
+                "de acordo com o exemplo a seguir: 23/06/2001.");
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MensagemDeErro manipularMensagegParaCPFRepetido() {
+
+        return new MensagemDeErro("Este CPF Já Está Cadastrado.");
+    }
+
 }
