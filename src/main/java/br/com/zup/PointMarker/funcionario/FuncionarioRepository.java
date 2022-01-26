f50c586f8e9bbf56a36d278af99f0310dc912f16
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-26 01:32:02 -0300

    Alterar tipo da chave na repository.

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioRepository.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioRepository.java
@@ -3,5 +3,5 @@ package br.com.zup.PointMarker.funcionario;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface FuncionarioRepository extends CrudRepository<Funcionario, String> {
+public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
 }
