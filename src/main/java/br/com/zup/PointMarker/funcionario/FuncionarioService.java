80c36949e2878d2ce6448df53288f613ed321275
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2022-01-26 13:47:56 -0300

    Atualizar o método para exibição de funcionáros na Service

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioService.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioService.java
@@ -1,16 +1,23 @@
 package br.com.zup.PointMarker.funcionario;
 
+import br.com.zup.PointMarker.enums.Status;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FuncionarioService {
 
-  @Autowired
+    @Autowired
     private FuncionarioRepository funcionarioRepository;
 
-  public Iterable<Funcionario> exibirFuncionarios(){
-    return funcionarioRepository.findAll();
-  }
+    public List<Funcionario> exibirFuncionarios(Status status) {
+        if (status != null){
+            return funcionarioRepository.findAllByStatus(status);
+        }
+
+        return (List<Funcionario>) funcionarioRepository.findAll();
+    }
 
 }
