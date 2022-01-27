a31a16f7f2e662e71cb65ced0c82aeb17d1dabea
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-27 12:28:40 -0300

    Adicionar pacote para o Resumo de saida do cadastro de horas no sistema.

--- src/main/java/br/com/zup/PointMarker/bancohoras/dtos/ResumoDTO/BancoDeHorasResumoDTO.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/dtos/ResumoDTO/BancoDeHorasResumoDTO.java
@@ -1,6 +1,6 @@
-package br.com.zup.PointMarker.bancohoras.dtos;
+package br.com.zup.PointMarker.bancohoras.dtos.ResumoDTO;
 
-import br.com.zup.PointMarker.funcionario.dtos.ResumoFuncionarioDTO;
+import br.com.zup.PointMarker.funcionario.dtos.ResumoDTO.ResumoFuncionarioDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
