ad7de8af80202c986e4f791b30d01ddad6cf601e
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-27 09:59:42 -0300

    Criar metodo para salvar banco de horas.

--- src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasService.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasService.java
@@ -1,7 +1,29 @@
 package br.com.zup.PointMarker.bancohoras;
 
+
+import br.com.zup.PointMarker.funcionario.Funcionario;
+import br.com.zup.PointMarker.funcionario.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class BancoDeHorasService {
+
+    @Autowired
+    private BancoDeHorasRepository bancoDeHorasRepository;
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+
+
+    public BancoDeHoras salvarHorasTrabalhadas(BancoDeHoras bancoDeHoras) {
+        Funcionario funcionario = funcionarioService.buscarFuncionario(bancoDeHoras.getId_funcionario().getId());
+
+            bancoDeHoras.setId_funcionario(funcionario);
+            return bancoDeHorasRepository.save(bancoDeHoras);
+    }
+
+
 }
