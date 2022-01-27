3e94546c97c3452a51e5a8f602ad8b791a3a7e82
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-27 09:54:42 -0300

    Alterar formato da entrada de datas e horas.

--- src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHoras.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHoras.java
@@ -1,6 +1,7 @@
 package br.com.zup.PointMarker.bancohoras;
 
 import br.com.zup.PointMarker.funcionario.Funcionario;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,9 +18,16 @@ public class BancoDeHoras {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private  int id;
+    private int id;
     @OneToOne
     private Funcionario id_funcionario;
+    private int horaExtra;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime entrada;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime saida;
 }
+// Hora extra vai ser do tipo int. Quando hora extra estiver com o número 0, o funcionario
+// Não tem hora extra, caso o funcionario coloque 1 na horaextra, ele colocará somente os valores de hora extra.
+//Ou seja, é possível fazer dois cadastros em um mesmo dia.
+// Todo cargo tem a sua cargo horária. Se passar Desta Cargo_Horaria, será considerado hora extra.
