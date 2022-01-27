7fb442bd7fb54fef0ada4f6488b00d99c12e6c85
evellyinhelenoir <evellyin.nunes@zup.com.br>
2022-01-27 10:26:17 -0300

    Criar construtor de autowired na controller de banco de horas.

--- src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasController.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasController.java
@@ -1,9 +1,22 @@
 package br.com.zup.PointMarker.bancohoras;
 
+import br.com.zup.PointMarker.funcionario.FuncionarioService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/bancohoras")
 public class BancoDeHorasController {
+
+    private FuncionarioService funcionarioService;
+
+    private ModelMapper modelMapper;
+
+    @Autowired
+    public BancoDeHorasController(FuncionarioService funcionarioService, ModelMapper modelMapper) {
+        this.funcionarioService = funcionarioService;
+        this.modelMapper = modelMapper;
+    }
 }
