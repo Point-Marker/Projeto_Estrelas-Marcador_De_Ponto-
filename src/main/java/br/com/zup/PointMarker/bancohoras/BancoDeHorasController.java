c10ae77c429266fc7ad13a81d82606d0cb4cd0b8
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-27 10:01:34 -0300

    Criar metodo cadastrar horas trabalhadas no sistema.

--- src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasController.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasController.java
@@ -1,9 +1,34 @@
 package br.com.zup.PointMarker.bancohoras;
 
+import br.com.zup.PointMarker.bancohoras.dtos.BancoDeHorasResumoDTO;
+import br.com.zup.PointMarker.funcionario.Funcionario;
+import br.com.zup.PointMarker.funcionario.dtos.ResumoFuncionarioDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/bancohoras")
 public class BancoDeHorasController {
+
+    @Autowired
+    private BancoDeHorasService horasService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    public BancoDeHorasResumoDTO cadastrarHorasTrabalhadas(@RequestBody BancoDeHoras bancoDeHoras) {
+        BancoDeHoras bancoDeHorasSalvo = horasService.salvarHorasTrabalhadas(bancoDeHoras);
+        Funcionario funcionario = bancoDeHorasSalvo.getId_funcionario();
+        ResumoFuncionarioDTO resumoFuncionarioDTO = modelMapper.map(funcionario, ResumoFuncionarioDTO.class);
+
+        BancoDeHorasResumoDTO resumoDTO = modelMapper.map(bancoDeHorasSalvo, BancoDeHorasResumoDTO.class);
+        resumoDTO.setFuncionario(resumoFuncionarioDTO);
+
+        return resumoDTO;
+    }
 }
