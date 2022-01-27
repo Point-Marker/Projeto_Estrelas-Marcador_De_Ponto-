5ef39167ef5bfc976be1c547970e1021f37b7c09
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-27 13:02:33 -0300

    Fix

--- src/main/java/br/com/zup/PointMarker/exceptions/FuncionarioComStatusInativo.java
+++
@@ -1,7 +0,0 @@
-package br.com.zup.PointMarker.exceptions;
-
-public class FuncionarioComStatusInativo extends RuntimeException {
-    public FuncionarioComStatusInativo(String message) {
-        super(message);
-    }
-}
