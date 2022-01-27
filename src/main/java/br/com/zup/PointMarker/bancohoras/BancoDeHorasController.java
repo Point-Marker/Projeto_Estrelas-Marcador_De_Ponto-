1303eeedf9ada03b1216ed45f455ebced474c83c
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2022-01-27 15:45:43 -0300

    Criar método para exibir banco de horas do funcionário na Controller

--- src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasController.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasController.java
@@ -1,9 +1,24 @@
 package br.com.zup.PointMarker.bancohoras;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/bancohoras")
 public class BancoDeHorasController {
+
+    @Autowired
+    private BancoDeHorasService bancoDeHorasService;
+
+    @GetMapping("/{id}")
+    public List<BancoDeHoras> exibirBancoDeHorasFuncionario(@PathVariable int id){
+        return bancoDeHorasService.exibirHorasTrabalhadas(id);
+    }
+
 }
