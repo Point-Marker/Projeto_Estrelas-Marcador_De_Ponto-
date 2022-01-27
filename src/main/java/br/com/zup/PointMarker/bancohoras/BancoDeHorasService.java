0e55dd6ad8cff3e8e5e77fa5a3f5fbd645fec368
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-27 10:05:31 -0300

    Criar metodo para validar as horas que estao sendo lancadas no sistema.

--- src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasService.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasService.java
@@ -6,6 +6,8 @@ import br.com.zup.PointMarker.funcionario.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BancoDeHorasService {
@@ -16,14 +18,54 @@ public class BancoDeHorasService {
     @Autowired
     private FuncionarioService funcionarioService;
 
+    int totalHorasTrabalhadas = 0;
 
 
     public BancoDeHoras salvarHorasTrabalhadas(BancoDeHoras bancoDeHoras) {
         Funcionario funcionario = funcionarioService.buscarFuncionario(bancoDeHoras.getId_funcionario().getId());
 
+        if (verificarHorasTrabalhadadas(bancoDeHoras.getId_funcionario().getId(), bancoDeHoras)) {
             bancoDeHoras.setId_funcionario(funcionario);
             return bancoDeHorasRepository.save(bancoDeHoras);
+        }
+
+        throw new RuntimeException("Você já excedeu as horas trabalhadas.");
     }
 
+    public boolean verificarHorasTrabalhadadas(int id, BancoDeHoras bancoDeHoras) {
+        List<BancoDeHoras> listaDeHorasTrabalhadas = new ArrayList<>();
 
+        for (BancoDeHoras referencia : listaDeHorasTrabalhadas) {
+            if (bancoDeHoras.getId() == id) {
+                listaDeHorasTrabalhadas.add(bancoDeHoras);
+                return true;
+            } else {
+                if (calcularHorasDeTrabalho(bancoDeHoras) > 10) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int calcularHorasDeTrabalho(BancoDeHoras bancoDeHoras) {
+        final int LIMITE_DE_HORAS_TRABALHADAS = 30;
+
+        int entrada = bancoDeHoras.getEntrada().getHour();
+        int saida = bancoDeHoras.getSaida().getHour();
+        int horasTrabalhadas = 0;
+        int capturarHoras = 0;
+
+        for (int i = entrada; i <= saida; i++) {
+            horasTrabalhadas += 1;
+            capturarHoras = horasTrabalhadas;
+        }
+        totalHorasTrabalhadas += capturarHoras;
+
+        if (totalHorasTrabalhadas <= LIMITE_DE_HORAS_TRABALHADAS) {
+            return totalHorasTrabalhadas;
+        }
+
+        throw new RuntimeException("Excedeu");
+    }
 }
