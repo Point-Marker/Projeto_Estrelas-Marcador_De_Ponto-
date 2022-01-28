9bbc840002ab7a4651b5700f0f84787a18259283
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-28 16:07:21 -0300

    Alterar atributos de saida dto.

--- src/main/java/br/com/zup/PointMarker/bancohoras/dtos/ResumoSaidaDTO/BancoDeHorasResumoDTO.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/dtos/ResumoSaidaDTO/BancoDeHorasResumoDTO.java
@@ -1,20 +1,24 @@
-package br.com.zup.PointMarker.bancohoras.dtos.ResumoDTO;
+package br.com.zup.PointMarker.bancohoras.dtos.ResumoSaidaDTO;
 
+import br.com.zup.PointMarker.funcionario.Funcionario;
 import br.com.zup.PointMarker.funcionario.dtos.ResumoDTO.ResumoFuncionarioDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class BancoDeHorasResumoDTO {
     private ResumoFuncionarioDTO funcionario;
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-    private LocalDateTime entrada;
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-    private LocalDateTime saida;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate diaDoTrabalho;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime entrada;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime saida;
 }
