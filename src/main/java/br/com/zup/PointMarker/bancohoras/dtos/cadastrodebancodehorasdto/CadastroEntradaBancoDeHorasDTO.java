9f61cea32d01c4b02e1edf98421a913ac142ec68
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-28 16:10:45 -0300

    Fix

---
+++ src/main/java/br/com/zup/PointMarker/bancohoras/dtos/cadastrodebancodehorasdto/CadastroEntradaBancoDeHorasDTO.java
@@ -0,0 +1,22 @@
+package br.com.zup.PointMarker.bancohoras.dtos.cadastrodebancodehorasdto;
+
+import br.com.zup.PointMarker.funcionario.Funcionario;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class CadastroEntradaBancoDeHorasDTO {
+    private Funcionario funcionario;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime entrada;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime saida;
+}
