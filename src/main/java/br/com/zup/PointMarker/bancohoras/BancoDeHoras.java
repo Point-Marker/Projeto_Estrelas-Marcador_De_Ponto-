88c33974222814bd51629100d2424d49e679b6a9
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-28 16:02:40 -0300

    Alterar atributos do Banco de horas.

--- src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHoras.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHoras.java
@@ -7,7 +7,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @NoArgsConstructor
 @Getter
@@ -20,14 +21,11 @@ public class BancoDeHoras {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @OneToOne
-    private Funcionario id_funcionario;
-    private int horaExtra;
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-    private LocalDateTime entrada;
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-    private LocalDateTime saida;
+    private Funcionario funcionario;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate diaDoTrabalho;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime entrada;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime saida;
 }
-// Hora extra vai ser do tipo int. Quando hora extra estiver com o número 0, o funcionario
-// Não tem hora extra, caso o funcionario coloque 1 na horaextra, ele colocará somente os valores de hora extra.
-//Ou seja, é possível fazer dois cadastros em um mesmo dia.
-// Todo cargo tem a sua cargo horária. Se passar Desta Cargo_Horaria, será considerado hora extra.
