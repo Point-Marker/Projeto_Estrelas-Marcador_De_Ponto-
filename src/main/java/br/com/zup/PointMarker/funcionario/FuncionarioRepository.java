1838a7e30b180da7101dd2ed9c671e8c84396307
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-01 15:05:07 -0300

    Criar metodo para buscar funcionario pelo username.

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioRepository.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioRepository.java
@@ -1,13 +1,15 @@
 package br.com.zup.PointMarker.funcionario;
 
 import br.com.zup.PointMarker.enums.Status;
-
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
 
     List<Funcionario> findAllByStatus(Status status);
+
+    Optional<Funcionario> findByNomeUsuario(String nomeUsuario);
 }
 
