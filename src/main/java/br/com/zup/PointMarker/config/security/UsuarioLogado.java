9a6a46fa4cefad0b2295bbe08c8423a922d86789
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-01 12:49:16 -0300

    Criar classe Usuario Logado com a implementacao da interface user details.

---
+++ src/main/java/br/com/zup/PointMarker/config/security/UsuarioLogado.java
@@ -0,0 +1,55 @@
+package br.com.zup.PointMarker.config.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UsuarioLogado implements UserDetails {
+    private int id;
+    private String nomeUsuario;
+    private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
