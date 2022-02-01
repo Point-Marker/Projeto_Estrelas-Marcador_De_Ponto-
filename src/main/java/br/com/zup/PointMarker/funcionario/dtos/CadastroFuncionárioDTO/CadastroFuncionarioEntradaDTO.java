665d59d62e1ec1ce5c24dd972e713254e348b3c7
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-01 11:45:25 -0300

    Incluir os campos nomeUsuario e senha na dto.

--- src/main/java/br/com/zup/PointMarker/funcionario/dtos/CadastroFuncionárioDTO/CadastroFuncionarioEntradaDTO.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/dtos/CadastroFuncionárioDTO/CadastroFuncionarioEntradaDTO.java
@@ -19,6 +19,11 @@ public class CadastroFuncionarioEntradaDTO {
     @NotBlank(message = "{nome.null}")
     @Size(min = 10, max = 30, message = "{nome.size}")
     private String nome;
+    @NotBlank(message ="{nomeUsuario.null}")
+    @Size(min = 4, max = 15, message = "{nomeUsuario.size}")
+    private String nomeUsuario;
+    @NotBlank(message = "{senha.null}")
+    private String senha;
     @CPF
     private String cpf;
     @JsonFormat(pattern = "dd/MM/yyyy")
