de812308e0e60ed601911172a0fa7de440c9cbba
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-01 23:54:13 -0300

    Criar metodo para gerar token.

--- src/main/java/br/com/zup/PointMarker/config/security/jwt/JWTComponent.java
+++ src/main/java/br/com/zup/PointMarker/config/security/jwt/JWTComponent.java
@@ -1,10 +1,26 @@
 package br.com.zup.PointMarker.config.security.jwt;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 public class JWTComponent {
     @Value("{jwt.segredo}")
     private String segredo;
     @Value("{jwt.tempo}")
     private Long milissegundo;
+
+    public String gerarToken(String username, int id){
+        Date vencimento = new Date(System.currentTimeMillis()*milissegundo);
+
+        String token = Jwts.builder().setSubject(username)
+                .claim("idUsuario", id).setExpiration(vencimento)
+                .signWith(SignatureAlgorithm.HS512, segredo.getBytes()).compact();
+        return token;
+    }
+
 }
