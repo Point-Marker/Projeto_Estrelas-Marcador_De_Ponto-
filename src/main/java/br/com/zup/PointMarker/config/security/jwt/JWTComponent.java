fe83082ea4f4f5522f7eb9689052e4c24e64c16f
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-02 00:22:21 -0300

    Criar metodo claim.

--- src/main/java/br/com/zup/PointMarker/config/security/jwt/JWTComponent.java
+++ src/main/java/br/com/zup/PointMarker/config/security/jwt/JWTComponent.java
@@ -1,21 +1,21 @@
 package br.com.zup.PointMarker.config.security.jwt;
 
+import br.com.zup.PointMarker.exceptions.TokenInvalidoException;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 public class JWTComponent {
-    @Value("{jwt.segredo}")
+    @Value("${jwt.segredo}")
     private String segredo;
-    @Value("{jwt.tempo}")
+    @Value("${jwt.tempo}")
     private Long milissegundo;
 
-    public String gerarToken(String username, int id){
-        Date vencimento = new Date(System.currentTimeMillis()*milissegundo);
+    public String gerarToken(String username, int id) {
+        Date vencimento = new Date(System.currentTimeMillis() * milissegundo);
 
         String token = Jwts.builder().setSubject(username)
                 .claim("idUsuario", id).setExpiration(vencimento)
@@ -23,4 +23,13 @@ public class JWTComponent {
         return token;
     }
 
+    public Claims pegarClaims(String token) {
+        try {
+            Claims claims = Jwts.parser().setSigningKey(segredo.getBytes())
+                    .parseClaimsJws(token).getBody();
+            return claims;
+        } catch (Exception e) {
+            throw new TokenInvalidoException();
+        }
+    }
 }
