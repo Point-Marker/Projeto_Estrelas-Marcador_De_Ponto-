68f608a541e54c7a4f22ed4ebb5da5092acb0296
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-02 02:06:28 -0300

    Sobrescrever metodo successfulAuthentication.

--- src/main/java/br/com/zup/PointMarker/config/security/JWT/FiltroDeAutenticacaoJWT.java
+++ src/main/java/br/com/zup/PointMarker/config/security/JWT/FiltroDeAutenticacaoJWT.java
@@ -1,5 +1,6 @@
 package br.com.zup.PointMarker.config.security.JWT;
 
+import br.com.zup.PointMarker.config.security.UsuarioLogado;
 import br.com.zup.PointMarker.config.security.UsuarioLogadoService;
 import br.com.zup.PointMarker.exceptions.AcessoNegadoException;
 import br.com.zup.PointMarker.funcionario.dtos.LoginDTO;
@@ -12,6 +13,8 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -32,11 +35,22 @@ public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilte
 
             UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                     login.getNomeUsuario(), login.getSenha(), userDetails.getAuthorities());
-
             Authentication autenticacao = authenticationManager.authenticate(authToken);
             return autenticacao;
         } catch (IOException e) {
             throw new AcessoNegadoException();
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        UsuarioLogado usuarioLogado = (UsuarioLogado) authResult.getPrincipal();
+        String username = usuarioLogado.getUsername();
+        int id = usuarioLogado.getId();
+
+        String token = jwtComponent.gerarToken(username, id);
+
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+        response.addHeader("Authorization", "Token " + token);
     }
 }
