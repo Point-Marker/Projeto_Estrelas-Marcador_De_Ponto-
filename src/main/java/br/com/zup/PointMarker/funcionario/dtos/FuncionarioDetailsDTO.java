a5dfd1069a5b6b40eea92ea67e6ffc4cce06466b
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-01 23:09:57 -0300

    Criar lista de banco de horas detalhado.

--- src/main/java/br/com/zup/PointMarker/funcionario/dtos/FuncionarioDetailsDTO.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/dtos/FuncionarioDetailsDTO.java
@@ -1,12 +1,15 @@
 package br.com.zup.PointMarker.funcionario.dtos;
 
+import br.com.zup.PointMarker.bancohoras.dtos.DetailsDTO.BancoDeHorasDetailsDTO;
 import br.com.zup.PointMarker.cargo.Cargo;
 import br.com.zup.PointMarker.enums.Status;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -16,9 +19,11 @@ public class FuncionarioDetailsDTO {
     private String nome;
     private String cpf;
     private double salario;
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataDeNascimento;
     private Cargo cargo;
     private Status status;
     private int totalHorasTrabalhadas;
     private int horasExtras;
+    private List<BancoDeHorasDetailsDTO> bancoDeHorasDetailsDTO;
 }
