ce81a0f986ebdd00a1830dab775e77a448c08ae0
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-04 00:49:13 -0300

    Criar usuario.

---
+++ src/main/java/br/com/zup/PointMarker/usuario/Usuario.java
@@ -0,0 +1,20 @@
+package br.com.zup.PointMarker.usuario;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nomeUsuario;
+    private String senha;
+}
