6a8685118e30fcab39fc0489c6d8039f539921db
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2022-02-04 11:26:01 -0300

    Criar classe de exceção para informar a carga horária

--- src/main/java/br/com/zup/PointMarker/exceptions/ASuaCargaHorariaException.java
+++ src/main/java/br/com/zup/PointMarker/exceptions/ASuaCargaHorariaException.java
@@ -1,2 +1,9 @@
-package br.com.zup.PointMarker.exceptions;public class ASuaCargaHorariaException {
+package br.com.zup.PointMarker.exceptions;
+
+public class ASuaCargaHorariaException extends RuntimeException {
+
+    public ASuaCargaHorariaException(String message) {
+        super(message);
+    }
+
 }
