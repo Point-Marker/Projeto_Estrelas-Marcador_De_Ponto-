36fc8d39d2cd140135731ec3a6112496974f40cd
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2022-02-07 16:09:13 -0300

    Criar exceção personalizada para status inválido no método exibirTodosFuncionarios

---
+++ src/main/java/br/com/zup/PointMarker/exceptions/StatusInvalidoException.java
@@ -0,0 +1,8 @@
+package br.com.zup.PointMarker.exceptions;
+
+public class StatusInvalidoException extends RuntimeException {
+    public StatusInvalidoException(String message) {
+        super(message);
+    }
+
+}
