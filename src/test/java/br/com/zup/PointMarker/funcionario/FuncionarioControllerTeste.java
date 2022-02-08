84834b24c688f98450349f08d2bfac6487139fa9
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-08 14:37:05 -0300

    Testar metodo de busca de funcionarios pelo id.

--- src/test/java/br/com/zup/PointMarker/funcionario/FuncionarioControllerTeste.java
+++ src/test/java/br/com/zup/PointMarker/funcionario/FuncionarioControllerTeste.java
@@ -3,23 +3,21 @@ package br.com.zup.PointMarker.funcionario;
 import br.com.zup.PointMarker.cargo.Cargo;
 import br.com.zup.PointMarker.component.Conversor;
 import br.com.zup.PointMarker.config.security.JWT.JWTComponent;
-import br.com.zup.PointMarker.config.security.UsuarioLogado;
 import br.com.zup.PointMarker.config.security.UsuarioLogadoService;
 import br.com.zup.PointMarker.enums.Status;
+import br.com.zup.PointMarker.exceptions.FuncionarioNaoEncontradoException;
 import br.com.zup.PointMarker.usuario.Usuario;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,7 +25,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.ArrayList;
 
 @WebMvcTest({FuncionarioController.class, JWTComponent.class, UsuarioLogadoService.class, Conversor.class})
 public class FuncionarioControllerTeste {
@@ -81,12 +78,14 @@ public class FuncionarioControllerTeste {
 
 
     @Test
-    @WithMockUser("Vander108")
-    public void testarExbicaoDeFuncionarioPeloId() throws Exception {
+    @WithMockUser(username = "Afonso", authorities = "USER")
+    public void testarExbicaoDeFuncionarioPeloId_QuandoOFuncionarioForEncontrado() throws Exception {
+        Mockito.when(funcionarioService.buscarFuncionario(1)).thenReturn(funcionario);
 
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/funcionario/{id}", 1)
-                        .header("Authorization","Token eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJWYW5kZXIxMDgiLCJBdXRvcml6YWNhbyI6W3siYXV0aG9yaXR5IjoiVVNFUiJ9XSwiaWRVc3VhcmlvIjo2LCJleHAiOjE2NDQzMjc3NjJ9.wLejAPeOqda5MWQZ4qypCp3HHJPxVtkVYCaWsBjMGm6VC9CYjebS4043OM2PfU5FFphQbw5UTAQ5EW1eEr8DAw")
-                        .contentType(MediaType.APPLICATION_JSON))
+        ResultActions resultActions = mockMvc.perform
+                        (MockMvcRequestBuilders.get("/funcionario/1")
+                                .header("Authorization", "xablau")
+                                .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
