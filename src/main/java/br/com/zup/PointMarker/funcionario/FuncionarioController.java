b00988b187e262db35ecb240817d0398a4a9fa20
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-09 01:21:51 -0300

    Incluir Service de usuario para buscar id do usuario logado.

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioController.java
@@ -1,10 +1,10 @@
 package br.com.zup.PointMarker.funcionario;
 
 import br.com.zup.PointMarker.funcionario.dtos.FuncionarioDetailsDTO;
+import br.com.zup.PointMarker.usuario.UsuarioService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,15 +16,18 @@ public class FuncionarioController {
 
     private ModelMapper modelMapper;
 
+    private UsuarioService usuarioService;
+
     @Autowired
-    public FuncionarioController(FuncionarioService funcionarioService, ModelMapper modelMapper) {
+    public FuncionarioController(FuncionarioService funcionarioService, ModelMapper modelMapper, UsuarioService usuarioService) {
         this.funcionarioService = funcionarioService;
         this.modelMapper = modelMapper;
+        this.usuarioService = usuarioService;
     }
 
-    @GetMapping("/{id}")
-    public FuncionarioDetailsDTO exibirFuncionarioId(@PathVariable int id) {
-        Funcionario idFuncionario = funcionarioService.buscarFuncionario(id);
+    @GetMapping()
+    public FuncionarioDetailsDTO exibirFuncionarioId() {
+        Funcionario idFuncionario = funcionarioService.buscarFuncionario(usuarioService.pegarId());
 
         return modelMapper.map(idFuncionario, FuncionarioDetailsDTO.class);
     }
