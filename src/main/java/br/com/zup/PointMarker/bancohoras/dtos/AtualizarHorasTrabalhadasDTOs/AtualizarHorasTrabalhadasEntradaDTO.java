62f8608f57658fea0d18e8e0855cd864fb6e3f20
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-10 10:43:27 -0300

    Incluir dto de cpf do funcionario.

--- src/main/java/br/com/zup/PointMarker/bancohoras/dtos/AtualizarHorasTrabalhadasDTOs/AtualizarHorasTrabalhadasEntradaDTO.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/dtos/AtualizarHorasTrabalhadasDTOs/AtualizarHorasTrabalhadasEntradaDTO.java
@@ -1,17 +1,20 @@
 package br.com.zup.PointMarker.bancohoras.dtos.AtualizarHorasTrabalhadasDTOs;
 
+import br.com.zup.PointMarker.funcionario.dtos.CpfDoFuncionarioDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.Valid;
 import java.time.LocalTime;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class AtualizarHorasTrabalhadasEntradaDTO {
-
+    @Valid
+    private CpfDoFuncionarioDTO funcionario;
     @JsonFormat(pattern = "HH:mm")
     private LocalTime entrada;
     @JsonFormat(pattern = "HH:mm")
