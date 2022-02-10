7b2a318b7031a559d4a63453b2983c2db5565452
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-10 10:42:42 -0300

    Criar metodo de busca de funcionario pelo cpf.

--- src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioRepository.java
+++ src/main/java/br/com/zup/PointMarker/funcionario/FuncionarioRepository.java
@@ -5,11 +5,14 @@ import br.com.zup.PointMarker.usuario.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
 
     List<Funcionario> findAllByStatus(Status status);
 
     Usuario findByUsuario(String nomeUsuario);
+
+    Optional<Funcionario> findByCpf(String cpf);
 }
 
