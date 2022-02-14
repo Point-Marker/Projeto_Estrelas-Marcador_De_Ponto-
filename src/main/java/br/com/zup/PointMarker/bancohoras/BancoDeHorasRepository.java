e870951a4f9d1dd47da9f7217da2478f796b8e1e
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-02-14 11:46:24 -0300

    Criar metodo de busca do dia do trabalho e do funcionario.

--- src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasRepository.java
+++ src/main/java/br/com/zup/PointMarker/bancohoras/BancoDeHorasRepository.java
@@ -1,10 +1,13 @@
 package br.com.zup.PointMarker.bancohoras;
 
 import br.com.zup.PointMarker.funcionario.Funcionario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface BancoDeHorasRepository extends CrudRepository<BancoDeHoras, Integer> {
 
@@ -13,4 +16,6 @@ public interface BancoDeHorasRepository extends CrudRepository<BancoDeHoras, Int
     BancoDeHoras findByDiaDoTrabalho(LocalDate diaDoTrabalho);
 
     List<BancoDeHoras> findAllByDiaDoTrabalho(LocalDate mesDeFiltro);
+
+    Optional<BancoDeHoras> findByDiaDoTrabalhoAndFuncionario(LocalDate diaDoTrabalho, Funcionario funcionario);
 }
